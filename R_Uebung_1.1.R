3ed52170be56cb1cd6da8affdaed6a5fe7c35075
lulu03pro <paulstrimtu@yahoo.de>
2023-10-25 02:21:13 +0200

    Uebung1

---
+++ R_Uebung_1.1.R
@@ -0,0 +1,52 @@
+12+6
+18+5
+20-5
+
+12*6
+12/6
+12.0/6
+
+2^3
+16^3
+
+sqrt(18)
+sqrt(64)
+
+exp(4)
+exp(1)
+
+3*(4+6)/(3+3)
+
+c(5,2,10,2,4)
+
+x <- c(5,2,10,2,4)
+x
+length(x)
+sum(x)
+x >= 4
+which(x>=4)
+max(x)
+which.max(x>=4)
+sort(x)
+unique(x)
+y <- unique(x)
+2*x
+2*y
+
+1:50
+
+seq(0,10,by=2)
+
+e <- sum(seq(0,1000,by=2))
+     sum(seq(0,1000,by=2))
+     
+o <- sum(seq(1,1000,by=2))
+     sum(seq(1,1000,by=2))
+     
+e+o
+sum(seq(0,1000,by=1))
+
+
+
+
+
